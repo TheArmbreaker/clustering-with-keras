182cd669ac6d367093925e5dda33dbc5ea202a9d
TheArmbreaker <ma133@hdm-stuttgart.de>
2023-04-16 19:47:03 +0200

    working selection of cluster samples and filenames

--- app.R
+++ app.R
@@ -20,9 +20,9 @@ df_clusters <- read.csv("2023_04_16_00_32_52_recipe_clusteredweapons.csv",header
 # Get Clusters for DropDown Selection
 cluster_vector <- df_clusters |> select(.cluster)
 
-df_img_files <- df_clusters |> filter(.cluster==cluster_vector[[1]][1]) |> select(myFiles)
-df_img_sample <- sample(df_img_files$myFiles,3)
-df_img <- data.frame(id = c(1:3), img_path = df_img_sample)
+# df_img_files <- df_clusters |> filter(.cluster==cluster_vector[[1]][1]) |> select(myFiles)
+# df_img_sample <- sample(df_img_files$myFiles,4)
+# df_img <- data.frame(id = c(1:4), img_path = df_img_sample)
 
 # Define UI
 ui <- fluidPage(theme = shinytheme("united"),
@@ -40,7 +40,7 @@ ui <- fluidPage(theme = shinytheme("united"),
                              h1("Header 1"),
                              
                              h4("Output 1"),
-                             verbatimTextOutput("txtout"),
+                             #verbatimTextOutput("txtout"),
                              
                            ) # mainPanel
                            
@@ -60,12 +60,14 @@ ui <- fluidPage(theme = shinytheme("united"),
                             #   column(width=4,imageOutput("myImage2"))
                                
                             # ),
-                             h4("Multiple Images in DIV container"),
-                            selectInput("clus",
-                                        "Cluster:",
+                            h4("Choose a cluster for sample images"),
+                            selectInput("var_clus",
+                                        "Choose a cluster:",
                                         cluster_vector,
                                         selected = 1),
-                             uiOutput("myImage5")
+                            h4("Filenames and Images"),
+                            verbatimTextOutput("filenames"),
+                            uiOutput("myImage5")
                            ) # mainPanel
                   ),
                   tabPanel("References", "This panel is intentionally left blank")
@@ -85,18 +87,61 @@ ui <- fluidPage(theme = shinytheme("united"),
 
 
 # Define server function  
-server <- function(input, output) {
+server <- function(input, output, session) {
   
-  output$txtout <- renderText({
-    paste( input$txt1, input$txt2, sep = " " )
+  img_data <- reactive({
+    df_img_files <- df_clusters |>
+      filter(.cluster==input$var_clus) |>
+      select(myFiles)
+    
+    df_img_sample <- sample(df_img_files$myFiles,4)
+    df_img <- data.frame(id = c(1:4), img_path = df_img_sample)
+    df_img
+  })
+  
+  output$filenames <- renderText({
+    myString=""
+    for (i in img_data()[[2]]){
+      myString <- paste0(myString,"\n",i)
+    }
+    substr(myString,2,nchar(myString))
   })
   
   output$futureData <- renderTable(
     {
       df_clusters |> count(.cluster) |>
-        filter(.cluster==input$clus)
+        filter(.cluster==input$var_clus)
     }
   )
+
+  observe({
+    for (i in 1:4)
+    {
+      print(i)
+      local({
+        my_i <- i
+        imagename = paste0(my_i)
+        print(imagename)
+        output[[imagename]] <-
+          renderImage({
+            list(src = file.path("weapons",img_data()$img_path[my_i]), 
+                 width = "240", height = "180",
+                 alt = "Image failed to render")
+          }, deleteFile = FALSE)
+      })
+    }
+  })
+  
+  output$myImage5 <- renderUI({
+    img_output_list <- 
+      lapply(1:4,
+             function(i){
+               imagename= paste0(i)
+               #imageOutput(i)
+               div(style = "display:inline-block;margin-bottom:-12em", imageOutput(imagename))
+             })
+    do.call(tagList,img_output_list)
+  })
   
   output$ggplot_cluster <- renderPlot(
     {
@@ -120,47 +165,7 @@ server <- function(input, output) {
            height = 180)
     },deleteFile = FALSE
   )
-  
-  output$myImage2 <- renderImage(
-    {
-      filename <- file.path("weapons/0a91369dacf3b60c.jpg")
-      list(src = filename,
-           alt = "Here should be an image.",
-           width = 240,
-           height = 180)
-    },deleteFile = FALSE
-  )
 
-  n <- nrow(df_img)
-  
-  observe({
-    for (i in 1:n)
-    {
-      print(i)
-      local({
-        my_i <- i
-        imagename = paste0(my_i)
-        print(imagename)
-        output[[imagename]] <-
-          renderImage({
-            list(src = file.path("weapons",df_img$img_path[my_i]), 
-                 width = "240", height = "180",
-                 alt = "Image failed to render")
-          }, deleteFile = FALSE)
-      })
-    }
-  })
-
-  output$myImage5 <- renderUI({
-    img_output_list <- 
-      lapply(1:n,
-             function(i){
-               imagename= paste0(i)
-               #imageOutput(i)
-               div(style = "display:inline-block;margin-bottom:-12em", imageOutput(imagename))
-             })
-    do.call(tagList,img_output_list)
-  })
 
 } # server
 
